7b866f58477b9c35478c4e35829201678af85a87
Certaingemstone <jade.chongsathapornpong@gmail.com>
2021-06-19 10:45:14 -0700

    Create botmain.py

---
+++ botmain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+Discord bot for Akmigone counting chat analysis. WIP.
+"""
+
+import os
+
+import discord
+from dotenv import load_dotenv
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
